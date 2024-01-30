8129feb9658ec430d30feaef3de30d503ab7ee82
Brenton Adey <brentonadey@gmail.com>
2024-01-30 08:43:58 +0800

    EEGNet training

--- arl-eegmodels/fit.py
+++ arl-eegmodels/fit.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 # enable syntax highlighting
 keras = tf.keras
 from keras import backend as K
-from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
 
 from EEGModels import EEGNet
 from utils import get_data
@@ -22,7 +22,7 @@ channels = 1
 samples = 3000
 
 model = EEGNet(nb_classes = 5, Chans = channels, Samples = samples, 
-               dropoutRate = 0.5, kernLength = 64, F1 = 8, D = 1, F2 = 16, 
+               dropoutRate = 0.5, kernLength = 64, F1 = 64, D = 1, F2 = 64, 
                dropoutType = 'Dropout')
 
 model.summary()
@@ -33,7 +33,7 @@ model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',
 # Make directories required
 os.makedirs("logs", exist_ok=True)
 os.makedirs("models", exist_ok=True)
-experiment_name = "initial"
+experiment_name = "initial2"
 os.makedirs(f"models/eegNet/{experiment_name}", exist_ok=True)
 
 logdir = f"logs/eegNet/{experiment_name}/{datetime.now().strftime('%Y%m%d-%H%M%S')}"
@@ -42,14 +42,21 @@ file_writer.set_as_default()
 
 callbacks = [
     ModelCheckpoint(
-        filepath="models/from_scratch/sgd/version-n{epoch:02d}.h5",
+        filepath="models/eegNet/initial2/version-n{epoch:02d}.h5",
         save_best_only=True),
     TensorBoard(
         log_dir=logdir,
         histogram_freq=1
+    ),
+    EarlyStopping(
+        monitor='val_loss',
+        patience=20,
+        start_from_epoch=50
     )
 ]
 
-fittedModel = model.fit(train_ds, epochs = 300, 
+fittedModel = model.fit(train_ds, epochs = 150, 
                         verbose = 2, validation_data=val_ds,
                         callbacks=callbacks, class_weight = class_weights)
+
+model.save("models/eegNet/initial2/trained_model.h5")
